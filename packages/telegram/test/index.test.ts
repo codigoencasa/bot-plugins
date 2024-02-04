46ae8afd0e39ed5f16cabf04bf532d9e3775f711
elimeleth <juancapuanolopez@gmail.com>
2024-02-04 12:53:07 -0400

    cambios generales para funcionamiento

--- packages/telegram/test/index.test.ts
+++ packages/telegram/test/index.test.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 import { describe } from 'node:test'
 
-import TelegramProvider from '../src/index'
+import {TelegramProvider} from '../src/index'
 
 describe('basics', () => {
   it('should pass', () => {
